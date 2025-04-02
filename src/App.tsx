54f1fb6d44384ca82686a41e042c2ef81d70527c
hanumant100 <hanumantverma.100@gmail.com>
2025-04-02 14:55:13 +0530

    changes : clerk  authenctication ansd routing setup cpmplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import { Button } from "./components/ui/button"
+
+import { BrowserRouter as Router,Routes ,Route } from "react-router-dom"
+
+
+import PublicLayout from "@/layouts/public-layout"
+import ProtectedRoutes from "@/layouts/Protected-routes"
+import AuthenticationLayout from "@/layouts/Auth-layout"
+import MainLayout from "@/layouts/main-layout"
+
+import HomePage from "@/Routes/home"
+import { SignInPage} from "./Routes/sign-in"
+import { SignUpPage } from "./Routes/sign-up"
+
 
 const App = () => {
   return (
-    <div className='text-blue-500'>
-    <Button variant={"destructive" } size={"icon"}>click me</Button>
-    </div>
+    
+  <Router>
+    <Routes>
+      {/* public routes */}
+      <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+
+        {/* {authentication routes} */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+
+      {/* protected routes */}
+      <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>} >
+      </Route>
+    </Routes>
+  </Router>
+    
   )
 }
 
-export default App
+export default App;
---
+++ src/layouts/main-layout.tsx
@@ -0,0 +1,22 @@
+import { Container } from "@/components/container";
+import { Outlet } from "react-router-dom";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+  
+       <Container className="flex-grow">
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        </Container>
+     
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
